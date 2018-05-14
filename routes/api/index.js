8bca677bb66fd9f5b9b2ce773d136ca1098280ee
04vibs <04vibs@gmail.com>
2018-05-14 13:31:57 +0530

    studentapi

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const route=require('express').Router()
 
 route.use('/courses',require('./course'))
+route.use('/students',require('./student'))
 
 exports=module.exports={
     route
---
+++ routes/api/student.js
@@ -0,0 +1,48 @@
+const Student=require('../../db').Student
+const route=require('express').Router();
+
+route.get('/',(req,res)=>{
+    console.log("Inside Student get")
+    Student.findAll()
+                .then((students)=>{
+                    res.status(200).send(students)
+                })
+                .catch((err)=>{
+                    error: "could not retrieve students"
+                })
+})
+
+route.get('/:id',(req,res)=>{
+    console.log("Inside student by id")
+    Student.findOne({
+    
+        where: {
+            id:req.params.id
+        }
+    
+    }).then((studentid)=>{
+        console.log(studentid)
+        res.status(200).send(studentid)
+    }).catch((err)=>{
+        error: "could not find student by corresponding id"
+    })
+})
+
+route.post('/',(req,res)=>{
+    console.log("Inside student post")
+    console.log(req.body.name)
+    Student.create({
+        name:req.body.name
+    }).then((students)=>{
+        console.log("Inside student creation post")
+        res.status(201).send(students)
+    }).catch((err)=>{
+        console.log("inside err of student")
+        res.status(501).send({
+            error:"could not add"
+        })
+    })
+})
+
+
+exports=module.exports=route
